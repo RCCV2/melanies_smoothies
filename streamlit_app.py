66e7fa4c35274a301240473ef7d79189f134ebbc
RCCV2 <162527764+RCCV2@users.noreply.github.com>
2024-03-07 15:24:24 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,10 @@ if ingredients_list:
 
     #if ingredients_string:
     if time_to_insert:
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered,  ' +name_on_order, icon="✅")
